8fb7f52b79d3efae36b8bb1f30b156bee5bc45f9
Nick Mohale <52918728+nickmohale-collab@users.noreply.github.com>
2024-02-09 20:25:05 +0200

    Adding Test GridView

--- AcuContipayWebApp/ProcessPayment.aspx.cs
+++ AcuContipayWebApp/ProcessPayment.aspx.cs
@@ -13,11 +13,38 @@ namespace AcuContipayWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!Page.IsPostBack)
+            if (!IsPostBack)
             {
-                //refreshdata();
+                // Create a dummy DataTable
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[10] {
+            new DataColumn("orderType", typeof(string)),
+            new DataColumn("orderNbr", typeof(string)),
+            new DataColumn("approved", typeof(string)),
+            new DataColumn("status", typeof(string)),
+            new DataColumn("orderDate", typeof(string)),
+            new DataColumn("CreatedByID_Creator_Username", typeof(string)),
+            new DataColumn("OrderQty", typeof(int)),
+            new DataColumn("CuryID", typeof(string)),
+            new DataColumn("CuryOrderTotal", typeof(decimal)),
+            new DataColumn("CuryUnpaidBalance", typeof(decimal))
+                         });
+
+                // Add some dummy data to the DataTable
+                dt.Rows.Add("Type1", "123", "Yes", "Approved", DateTime.Now.ToShortDateString(), "User1", 5, "USD", 100.00, 50.00);
+                dt.Rows.Add("Type2", "456", "No", "Pending", DateTime.Now.ToShortDateString(), "User2", 10, "EUR", 200.00, 150.00);
+
+                // Bind the DataTable to the GridView
+                gvOpenOrders.DataSource = dt;
+                gvOpenOrders.DataBind();
             }
         }
+
+        protected void gvSelIdxChanged(object sender, EventArgs e)
+        {
+            // Your event handler logic here, if any
+        }
+
 
         public void refreshdata()
         {
